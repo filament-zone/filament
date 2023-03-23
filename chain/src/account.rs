c2bc7e2190b977b23ad85b0f5974c825a15678ac
patternmachine <patternmachine@protonmail.com>
2023-03-23 22:48:21 +0100

    feat(spec): add account to spec

--- chain/src/account.rs
+++ chain/src/account.rs
@@ -7,7 +7,9 @@ pub enum Account {
     Single {
         address: Address,
 
+        /// Sytem wide unique identifier
         id: u64,
+        /// Account level nonce
         sequence: u64,
     },
 }
